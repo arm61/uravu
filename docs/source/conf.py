f582072514e4478f5caf0e4a7d055e5f10dc277f
Andrew McCluskey <andrew.mccluskey@diamond.ac.uk>
2020-05-20 11:12:38 +0100

    version number bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Andrew R. McCluskey'
 author = 'Andrew R. McCluskey'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.7'
 
 
 # -- General configuration ---------------------------------------------------
