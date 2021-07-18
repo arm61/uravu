75f79eb2df4bd840b515831dca45c8dc75ca4576
Andrew McCluskey <arm61@users.noreply.github.com>
2021-07-18 19:04:35 +0200

    use version for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,8 @@ copyright = '2020, Andrew R. McCluskey'
 author = 'Andrew R. McCluskey'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.2'
+from uravu import __version__
+release = __version__
 
 
 # -- General configuration ---------------------------------------------------
