bc67a56254ead6c0c544e1da73320ca422f21a59
arm61 <andrew@mccluskey.scot>
2022-07-25 14:49:27 +0200

    Remove rogue import

--- uravu/tests/test_distribution.py
+++ uravu/tests/test_distribution.py
@@ -7,7 +7,6 @@ Tests for distribution module
 # author: Andrew R. McCluskey
 
 
-from math import dist
 import unittest
 import numpy as np
 from numpy.testing import assert_almost_equal, assert_equal
